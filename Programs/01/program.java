73f4a006eef77a74bb06797f7e749516a5401430
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-18 12:29:42 +0530

    Update program.java

--- Programs/01/program.java
+++ Programs/01/program.java
@@ -2,7 +2,17 @@ class program
 {
     public static void main (String args[])
     {
+        //Program One For Single Line Print Statement
+
         System.out.println("Hello Java");
-        System.out.println("First Program");
     }
 }
+
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programone  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
