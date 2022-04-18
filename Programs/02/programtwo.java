a85b6d3558347a383068af7413e99da9b08754b1
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-18 12:20:21 +0530

    Create programtwo.java

---
+++ Programs/02/programtwo.java
@@ -0,0 +1,19 @@
+class programtwo
+{
+    public static void main (String args[])
+    {
+
+        // Program Two For Multi Line Print Statement
+
+        System.out.println("Hello Java");
+        System.out.println("Second Program");
+    }
+}
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programtwo  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
