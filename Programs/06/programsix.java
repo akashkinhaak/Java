d7ffc072344eb3b80ed787b180825d73573f085c
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-19 17:34:08 +0530

    Create programsix.java

---
+++ Programs/06/programsix.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class programsix
+{
+  public static void main(String args[])
+  {
+      int number;
+
+      System.out.println("Enter Number to multiply with 2 ");
+        
+      Scanner s = new Scanner(System.in);
+
+      number= s.nextInt();
+    
+      System.out.println(2*number);
+  }
+}
+
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programsix  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
+
