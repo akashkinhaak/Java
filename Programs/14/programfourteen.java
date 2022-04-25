11d2da80611c331e3e04e6a65e1e239b08659495
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-25 19:59:54 +0530

    Create programfourteen.java

---
+++ Programs/14/programfourteen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class programfourteen
+{
+    public static void main(String[] args)
+    {
+        int num = 1, s =0;
+        while (num<=10)
+        {
+            s+=num;
+            ++num;
+
+        }
+        System.out.println("Sum Of First 10 Natural Numbers is : "+s);
+    }
+}
+// Program OF WHILE
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programfourteen  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
