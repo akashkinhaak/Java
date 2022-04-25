350fbb8b77b7e95c1c3889472052e26e2f618d35
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-25 20:10:13 +0530

    Create programeighteen.java

---
+++ Programs/18/programeighteen.java
@@ -0,0 +1,28 @@
+public class programeighteen
+
+{
+
+        public static void main(String[] args)
+        {
+            int i,s=0;
+
+            for(i=1;i<=10;i++)
+
+                s+=i;
+
+            System.out.println("Sum of first 10 natural numbers is:" +s);
+
+        }
+    }
+
+    // Program OF FOR Loop
+
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programeighteen  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
+
