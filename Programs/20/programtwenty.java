afbc93a78c379f3d50055c1fa182814d4a6cfcc9
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-25 20:15:16 +0530

    Create programtwenty.java

---
+++ Programs/20/programtwenty.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class programtwenty
+{
+
+
+
+
+    public static void main(String[] args) {
+        int x,y,z;
+        for(x=1;x<=3;x++)
+        {
+            for(y=2;y<=3;y++)
+            {
+                z=x*x*x+y*y*y;
+                System.out.println(x+ " " + y + " " +z);
+            }
+        }
+    }
+}
+
+// Nested Loop program
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programtwenty  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
