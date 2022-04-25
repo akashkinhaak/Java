572147f0bf736b5e188b9d4f8dbe94689e08ca93
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-04-25 20:04:37 +0530

    Create programsixteen.java

---
+++ Programs/16/programsixteen.java
@@ -0,0 +1,24 @@
+public class programsixteen
+{
+    public static void main(String[] args) {
+        int num=1,s=0;
+
+        do {
+            s+=num;
+            ++num;
+
+        }
+        while (num<=10);
+        System.out.println("Sum Of First 10 Natural Numbers is : "+s);
+
+    }
+}
+
+// Program Of Do While
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program i.e. programsixteen  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
