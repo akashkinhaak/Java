266f4189f56f1e0b8909217de9128618ab24eb88
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-05-20 18:02:54 +0530

    Create packagepr3.java

---
+++ Programs/package03/packagepr3.java
@@ -0,0 +1,21 @@
+import packagesources.sourceclass;
+
+public class packagepr3
+{
+    public static  void main(String[] args)
+    {
+        packagesources.sourceclass obj = new packagesources.sourceclass();
+        double ra = obj.area(19,11);
+        System.out.println("Area of Rectangle is : "+ ra);
+    }
+}
+
+
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program/Class  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
