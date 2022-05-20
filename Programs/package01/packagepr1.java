71caa6ddfcf4ef9283d7f2b5f67adc2718ac2270
Akash <68741152+akashkinhaak@users.noreply.github.com>
2022-05-20 17:59:43 +0530

    Create packagepr1.java

---
+++ Programs/package01/packagepr1.java
@@ -0,0 +1,21 @@
+import packagesources.*;
+
+public class packagepr1
+{
+    public static  void main(String[] args)
+    {
+        sourceclass obj = new sourceclass();
+        double carea = obj.area(10,10);
+        System.out.println("Area of circle is : "+carea);
+    }
+}
+
+
+
+// Change Run Configuration ClassName To the program name while running the Program
+
+// Step-1 Click On Configuration Icon Left To Run Button
+// Step-2 Click On Edit Configuration
+// Step-3 In Build and Run Section Type Name OF Program/Class  and Click on Apply then OK
+// Step-4 Run Program Successfully
+// Originally Developed BY Akash
